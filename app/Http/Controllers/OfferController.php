8bee7ac19929efd1b0896460999e5b10a6bbf07a
James Freeman <jmcfreeman@gmail.com>
2020-12-10 19:58:21 -0500

    feat: add offers API route

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class OfferController extends Controller
 {
-    //
+    public function list(): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'offers' => \App\Models\Offer::paginate(20),
+        ]);
+    }
 }
